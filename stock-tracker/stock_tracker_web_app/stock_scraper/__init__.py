f67628d6465bc5c0fa64dbb9bc9294b7dee26394
Prakhar Agrawal <59526998+agrawal-prakhar@users.noreply.github.com>
2024-07-21 18:41:54 -0700

    Update __init__.py

--- stock-tracker/stock_tracker_web_app/stock_scraper/__init__.py
+++ stock-tracker/stock_tracker_web_app/stock_scraper/__init__.py
@@ -1,27 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+from .scrape_data import get_stock_data, save_data_to_csv
+from .config import TICKERS, SCHEDULE_TIME, CSV_FILENAME
 
-def get_stock_data(ticker):
-    url = f'https://finance.yahoo.com/quote/{ticker}'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    data = {}
-    data['ticker'] = ticker
-    data['price'] = soup.find('fin-streamer', {'data-field': 'regularMarketPrice'}).text
-    data['change'] = soup.find('fin-streamer', {'data-field': 'regularMarketChangePercent'}).text
-    data['volume'] = soup.find('td', {'data-test': 'TD_VOLUME-value'}).find('span').text
-
-    return data
-
-tickers = ['AAPL', 'GOOGL', 'MSFT']
-stock_data = []
-
-for ticker in tickers:
-    stock_data.append(get_stock_data(ticker))
-
-df = pd.DataFrame(stock_data)
-print(df)
-
-df.to_csv('stock_data.csv', index=False)
+__all__ = ['get_stock_data', 'save_data_to_csv', 'TICKERS', 'SCHEDULE_TIME', 'CSV_FILENAME']
